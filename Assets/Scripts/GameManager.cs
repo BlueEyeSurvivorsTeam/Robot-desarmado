65548061e4c1496ae5917d378186d6c15d2966ff
Max5hadow <max.duque.h@mail.pucv.cl>
2025-11-21 13:30:23 -0300

    0.5.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
@@ -17,7 +19,46 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
     }
-
+    public IEnumerator Open(GameObject panel, float duration)
+    {
+        float elapsedTime = 0f;
+        panel.transform.localScale = Vector3.zero;
+        panel.SetActive(true);
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+            panel.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, t);
+            yield return null;
+        }
+        panel.transform.localScale = Vector3.one;
+    }
+    public IEnumerator Close(GameObject panel, float duration)
+    {
+        float elapsedTime = 0f;
+        Vector3 initialScale = panel.transform.localScale;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+            panel.transform.localScale = Vector3.Lerp(initialScale, Vector3.zero, t);
+            yield return null;
+        }
+        panel.transform.localScale = Vector3.zero;
+        panel.SetActive(false);
+    }
+    public IEnumerator TransitionClose(Animator anim)
+    {
+        if(anim != null) anim.SetTrigger("Transition");
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+        Application.Quit();
+    }
+    public IEnumerator TransitionScene(string nameScene, Animator anim)
+    {
+        if (anim != null) anim.SetTrigger("Transition");
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(nameScene);
+    }
     public void SetPause(bool boolean)
     {
         isPaused = boolean;
--- Assets/Scripts/TransitionController.cs
+++ Assets/Scripts/TransitionController.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 public class TransitionController : MonoBehaviour
 {
     public float duration;
     public Animator anim;
     public void CallClose(GameObject panel)
     {
-        StartCoroutine(Close(panel));
+        if(GameManager.Instance != null) StartCoroutine(GameManager.Instance.Close(panel, duration));
     }
     public void CallOpen(GameObject panel)
     {
-        StartCoroutine(Open(panel));
+        if(GameManager.Instance != null) StartCoroutine(GameManager.Instance.Open(panel, duration));
     }
     public void SetPause(bool pause)
     {
@@ -22,52 +21,12 @@ public class TransitionController : MonoBehaviour
         if(state) Cursor.lockState = CursorLockMode.Locked;
         else Cursor.lockState = CursorLockMode.None;
     }
-    IEnumerator Open(GameObject panel)
-    {
-        float elapsedTime = 0f;
-        panel.transform.localScale = Vector3.zero;
-        panel.SetActive(true);
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / duration);
-            panel.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, t);
-            yield return null;
-        }
-        panel.transform.localScale = Vector3.one;
-    }
-    IEnumerator Close(GameObject panel)
-    {
-        float elapsedTime = 0f;
-        Vector3 initialScale = panel.transform.localScale;
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / duration);
-            panel.transform.localScale = Vector3.Lerp(initialScale, Vector3.zero, t);
-            yield return null;
-        }
-        panel.transform.localScale = Vector3.zero;
-        panel.SetActive(false);
-    }
-    private IEnumerator TransitionClose()
-    {
-        if(anim != null) anim.SetTrigger("Transition");
-        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
-        Application.Quit();
-    }
-    private IEnumerator TransitionScene(string nameScene)
-    {
-        if (anim != null) anim.SetTrigger("Transition");
-        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
-        SceneManager.LoadScene(nameScene);
-    }
     public void LoadScene(string nameScene)
     {
-        StartCoroutine(TransitionScene(nameScene));
+        if(GameManager.Instance != null) StartCoroutine(GameManager.Instance.TransitionScene(nameScene, anim));
     }
     public void CloseGame()
     {
-        StartCoroutine(TransitionClose());
+        if(GameManager.Instance != null) StartCoroutine(GameManager.Instance.TransitionClose(anim));
     }
 }
